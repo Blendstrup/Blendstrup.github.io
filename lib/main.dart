9a4bcc66d5c38790ce9f923ea106c1229dc45f68
Blendstrup <36919652+Blendstrup@users.noreply.github.com>
2020-11-12 17:34:04 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jonas Blendstrup Rasmussen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Jonas Blendstrup Rasmussen',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Text(
+            'A work in progress... Check back soon!',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+    );
+  }
+}
